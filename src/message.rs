0106ee4095d963691f54a0c9156f784f800e09da
Patrick Barrett <patrick@mkii.org>
2016-03-15 22:33:33 -0500

    change option interface; working, needs cleaning probably

--- src/message.rs
+++ src/message.rs
@@ -158,117 +158,291 @@ impl Code {
 
 pub mod option {
     #[derive(PartialEq, Eq, Debug)]
-    pub struct Option {
-        pub number: Number,
-        pub value: Vec<u8>
-    }
-    #[derive(PartialEq, Eq, Debug)]
-    #[repr(u16)]
-    pub enum Number {
-        ReservedOrUnknown,
-        IfMatch,
-        UriHost,
-        ETag,
+    pub enum Option {
+        IfMatch(Vec<u8>),
+        UriHost(String),
+        ETag(Vec<u8>),
         IfNoneMatch,
-        Observe,
-        UriPort,
-        LocationPath,
-        UriPath,
-        ContentFormat,
-        MaxAge,
-        UriQuery,
-        Accept,
-        LocationQuery,
-        ProxyUri,
-        ProxyScheme,
-        Size1,
-        NoResponse,
-        Unknown(u16)
+        Observe(u32),
+        UriPort(u16),
+        LocationPath(String),
+        UriPath(String),
+        ContentFormat(u16),
+        MaxAge(u32),
+        UriQuery(String),
+        Accept(u16),
+        LocationQuery(String),
+        ProxyUri(String),
+        ProxyScheme(String),
+        Size1(u32),
+        NoResponse(u8),
+        Unknown((u16, Vec<u8>))
     }
 
-    impl Number {
-        pub fn from_u16(raw_number: u16) -> Number {
-            match raw_number {
-                0 => Number::ReservedOrUnknown,
-                1 => Number::IfMatch,
-                3 => Number::UriHost,
-                4 => Number::ETag,
-                5 => Number::IfNoneMatch,
-                6 => Number::Observe,
-                7 => Number::UriPort,
-                8 => Number::LocationPath,
-                11 => Number::UriPath,
-                12 => Number::ContentFormat,
-                14 => Number::MaxAge,
-                15 => Number::UriQuery,
-                17 => Number::Accept,
-                20 => Number::LocationQuery,
-                35 => Number::ProxyUri,
-                39 => Number::ProxyScheme,
-                60 => Number::Size1,
-                284 => Number::NoResponse,
-                _ => Number::Unknown(raw_number)
+    impl Option {
+        pub fn build_header(&self, last_option_number: &mut u16) -> Vec<u8> {
+            let mut header = vec![0u8];
+
+            if self.number() < *last_option_number {
+                panic!("bad order");
+            }
+
+            let delta = self.number() - *last_option_number;
+            let base_delta = match delta {
+                0...12 => delta,
+                13...268 => {header.push((delta-13) as u8); 13},
+                269...64999 => {header.push(((delta-269) >> 8) as u8); header.push((delta-269) as u8); 14},
+                _ => unreachable!(),
+            } as u8;
+            let length = self.value_len();
+            let base_length = match length {
+                0...12 => length,
+                13...268 => {header.push((length-13) as u8); 13},
+                269...64999 => {header.push(((length-269) >> 8) as u8); header.push((length-269) as u8); 14},
+                _ => panic!("option too big"),
+            } as u8;
+
+            header[0] = base_delta << 4 | base_length;
+
+            *last_option_number = *last_option_number + delta;
+
+            header
+        }
+
+        pub fn value_len(&self) -> usize {
+            match *self {
+                Option::IfMatch(ref v) => (v).len(),
+                Option::UriHost(ref s) => s.len(),
+                Option::ETag(ref v) => v.len(),
+                Option::IfNoneMatch => 0,
+                Option::Observe(n) => Self::integer_as_bytes(n as u64).len(),
+                Option::UriPort(n) => Self::integer_as_bytes(n as u64).len(),
+                Option::LocationPath(ref s) => s.len(),
+                Option::UriPath(ref s) => s.len(),
+                Option::ContentFormat(n) => Self::integer_as_bytes(n as u64).len(),
+                Option::MaxAge(n) => Self::integer_as_bytes(n as u64).len(),
+                Option::UriQuery(ref s) => s.len(),
+                Option::Accept(n) => Self::integer_as_bytes(n as u64).len(),
+                Option::LocationQuery(ref s) => s.len(),
+                Option::ProxyUri(ref s) => s.len(),
+                Option::ProxyScheme(ref s) => s.len(),
+                Option::Size1(n) => Self::integer_as_bytes(n as u64).len(),
+                Option::NoResponse(n) => Self::integer_as_bytes(n as u64).len(),
+                Option::Unknown((_, ref v)) => v.len()
             }
         }
 
-        pub fn as_u16(&self) -> u16 {
+        pub fn value_as_bytes(&self) -> Vec<u8> {
+            match *self {
+                Option::IfMatch(ref v) => v.clone(),
+                Option::UriHost(ref s) => s.clone().into_bytes(),
+                Option::ETag(ref v) => v.clone(),
+                Option::IfNoneMatch => vec![],
+                Option::Observe(n) => Self::integer_as_bytes(n as u64),
+                Option::UriPort(n) => Self::integer_as_bytes(n as u64),
+                Option::LocationPath(ref s) => s.clone().into_bytes(),
+                Option::UriPath(ref s) => s.clone().into_bytes(),
+                Option::ContentFormat(n) => Self::integer_as_bytes(n as u64),
+                Option::MaxAge(n) => Self::integer_as_bytes(n as u64),
+                Option::UriQuery(ref s) => s.clone().into_bytes(),
+                Option::Accept(n) => Self::integer_as_bytes(n as u64),
+                Option::LocationQuery(ref s) => s.clone().into_bytes(),
+                Option::ProxyUri(ref s) => s.clone().into_bytes(),
+                Option::ProxyScheme(ref s) => s.clone().into_bytes(),
+                Option::Size1(n) => Self::integer_as_bytes(n as u64),
+                Option::NoResponse(n) => Self::integer_as_bytes(n as u64),
+                Option::Unknown((_, ref v)) => v.clone()
+            }
+        }
+
+        fn integer_as_bytes(mut n: u64) -> Vec<u8> {
+            let mut bytes = vec![];
+            while n > 0 {
+                bytes.push((n & 0xFF) as u8);
+                n = n >> 8;
+            }
+
+            bytes
+        }
+
+        pub fn from_raw(number: u16, value: &[u8]) -> Option {
+            let parsed_value = match format::get_by_number(number) {
+                format::Format::Empty => Self::should_be_empty(value),
+                format::Format::Opaque(min, max) => Self::should_be_opaque(value, min, max),
+                format::Format::UInt(min, max) => Self::should_be_uint(value, min, max),
+                format::Format::String(min, max) => Self::should_be_string(value, min, max),
+            };
+
+            match (number, parsed_value) {
+                (1, value::Value::Opaque(v)) => Option::IfMatch(v),
+                (3, value::Value::String(v)) => Option::UriHost(v),
+                (4, value::Value::Opaque(v)) => Option::ETag(v),
+                (5, value::Value::Empty) => Option::IfNoneMatch,
+                (6, value::Value::UInt(v)) => Option::Observe(v as u32),
+                (7, value::Value::UInt(v)) => Option::UriPort(v as u16),
+                (8, value::Value::String(v)) => Option::LocationPath(v),
+                (11, value::Value::String(v)) => Option::UriPath(v),
+                (12, value::Value::UInt(v)) => Option::ContentFormat(v as u16),
+                (14, value::Value::UInt(v)) => Option::MaxAge(v as u32),
+                (15, value::Value::String(v)) => Option::UriQuery(v),
+                (17, value::Value::UInt(v)) => Option::Accept(v as u16),
+                (20, value::Value::String(v)) => Option::LocationQuery(v),
+                (35, value::Value::String(v)) => Option::ProxyUri(v),
+                (39, value::Value::String(v)) => Option::ProxyScheme(v),
+                (60, value::Value::UInt(v)) => Option::Size1(v as u32),
+                (284, value::Value::UInt(v)) => Option::NoResponse(v as u8),
+                (_, value::Value::Opaque(v)) => Option::Unknown((number, v)),
+                _ => panic!("unhandled option number, format combination")
+            }
+        }
+
+        pub fn should_be_empty(value: &[u8]) -> value::Value {
+            match value.len() {
+                0 => value::Value::Empty,
+                _ => value::Value::Opaque(value.to_vec())
+            }
+        }
+
+        pub fn should_be_string(value: &[u8], min: u16, max: u16) -> value::Value {
+            match String::from_utf8(value.to_vec()) {
+                Ok(s) => value::Value::String(s),
+                Err(_) => value::Value::Opaque(value.to_vec())
+            }
+        }
+
+        pub fn should_be_uint(value: &[u8], min: u16, max: u16) -> value::Value {
+            if value.len() >= min as usize && value.len() <= max as usize {
+                let mut num: u64 = 0;
+                for byte in value {
+                    num = (num << 8) | *byte as u64;
+                };
+                value::Value::UInt(num)
+            } else {
+                value::Value::Opaque(value.to_vec())
+            }
+        }
+
+
+        pub fn should_be_opaque(value: &[u8], min: u16, max: u16) -> value::Value {
+            return value::Value::Opaque(value.to_vec());
+        }
+
+        pub fn number(&self) -> u16 {
             match *self{
-                Number::ReservedOrUnknown => 0,
-                Number::IfMatch => 1,
-                Number::UriHost => 3,
-                Number::ETag => 4,
-                Number::IfNoneMatch => 5,
-                Number::Observe => 6,
-                Number::UriPort => 7,
-                Number::LocationPath => 8,
-                Number::UriPath => 11,
-                Number::ContentFormat => 12,
-                Number::MaxAge => 14,
-                Number::UriQuery => 15,
-                Number::Accept => 17,
-                Number::LocationQuery => 20,
-                Number::ProxyUri => 35,
-                Number::ProxyScheme => 39,
-                Number::Size1 => 60,
-                Number::NoResponse => 284,
-                Number::Unknown(n) => n
+                Option::IfMatch(_) => 1,
+                Option::UriHost(_) => 3,
+                Option::ETag(_) => 4,
+                Option::IfNoneMatch => 5,
+                Option::Observe(_) => 6,
+                Option::UriPort(_) => 7,
+                Option::LocationPath(_) => 8,
+                Option::UriPath(_) => 11,
+                Option::ContentFormat(_) => 12,
+                Option::MaxAge(_) => 14,
+                Option::UriQuery(_) => 15,
+                Option::Accept(_) => 17,
+                Option::LocationQuery(_) => 20,
+                Option::ProxyUri(_) => 35,
+                Option::ProxyScheme(_) => 39,
+                Option::Size1(_) => 60,
+                Option::NoResponse(_) => 284,
+                Option::Unknown((n, _)) => n
             }
         }
 
         pub fn is_critical(&self) -> bool {
-            self.as_u16() & 0x01 != 0
+            self.number() & 0x01 != 0
         }
 
         pub fn is_elective(&self) -> bool {
-            self.as_u16() & 0x01 == 0
+            self.number() & 0x01 == 0
         }
 
         pub fn is_unsafe_to_forward(&self) -> bool {
-            self.as_u16() & 0x02 != 0
+            self.number() & 0x02 != 0
         }
 
         pub fn is_safe_to_forward(&self) -> bool {
-            self.as_u16() & 0x02 == 0
+            self.number() & 0x02 == 0
         }
 
         pub fn is_no_cache_key(&self) -> bool {
-            self.as_u16() & 0x1e == 0x1c
+            self.number() & 0x1e == 0x1c
         }
 
         pub fn is_cache_key(&self) -> bool {
-            self.as_u16() & 0x1e != 0x1c
+            self.number() & 0x1e != 0x1c
         }
     }
 
-    pub fn new(number: Number, value: Vec<u8>) -> Option {
-        Option{
-            number: number,
-            value: value
+    pub mod value {
+        use message::Error;
+
+        pub enum Value {
+            Empty,
+            Opaque(Vec<u8>),
+            String(String),
+            UInt(u64)
         }
     }
 
-    pub fn from_u16(numeric: u16, value: Vec<u8>) -> Option {
-        new(Number::from_u16(numeric), value)
+    pub mod format {
+        use message::Error;
+
+        pub enum Format {
+            Empty,
+            Opaque(u16,u16),
+            String(u16,u16),
+            UInt(u16,u16)
+        }
+
+/*
+        pub fn get_format(option: Option) {
+            match option {
+                Option::UriHost(_) => Format::Opaque(0, 8),
+                Option::UriHost(_) => Format::String(1, 255),
+                Option::ETag(_) => Format::Opaque(0, 0),
+                Option::IfNoneMatch(_) => Format::Empty,
+                Option::Observe(_) => Format::UInt(0, 4), //guess
+                Option::UriPort(_) => Format::UInt(0, 2),
+                Option::LocationPath(_) => Format::String(0, 255),
+                Option::UriPath(_) => Format::String(0, 255),
+                Option::ContentFormat(_) => Format::UInt(0, 2),
+                Option::MaxAge(_) => Format::UInt(0, 4),
+                Option::UriQuery(_) => Format::String(0, 255),
+                Option::Accept(_) => Format::UInt(0, 2),
+                Option::LocationQuery(_) => Format::String(0, 255),
+                Option::ProxyUri(_) => Format::String(0, 1034),
+                Option::ProxyScheme(_) => Format::String(0, 255),
+                Option::Size1(_) => Format::UInt(0, 4),
+                Option::NoResponse(_) => Format::UInt(0, 1),
+                Option::Unknown(_) => Format::Opaque(0, 65535)
+            }
+        }
+*/
+
+        pub fn get_by_number(number: u16) -> Format {
+            match number {
+                1 => Format::Opaque(0, 8),
+                3 => Format::String(1, 255),
+                4 => Format::Opaque(0, 0),
+                5 => Format::Empty,
+                6 => Format::UInt(0, 4), //guess
+                7 => Format::UInt(0, 2),
+                8 => Format::String(0, 255),
+                11 => Format::String(0, 255),
+                12 => Format::UInt(0, 2),
+                14 => Format::UInt(0, 4),
+                15 => Format::String(0, 255),
+                17 => Format::UInt(0, 2),
+                20 => Format::String(0, 255),
+                35 => Format::String(0, 1034),
+                39 => Format::String(0, 255),
+                60 => Format::UInt(0, 4),
+                284 => Format::UInt(0, 1),
+                _ => Format::Opaque(0, 65535)
+            }
+        }
     }
 }
 
@@ -333,7 +507,7 @@ impl Message {
             }
 
             if pkt.len() >= i+(length as usize) {
-                options.push(option::from_u16(option_number,pkt[i..i+(length as usize)].to_vec()));
+                options.push(option::Option::from_raw(option_number, &pkt[i..i+(length as usize)]));
             } else {
                 return Err(Error::MessageFormat);
             }
@@ -367,9 +541,9 @@ impl Message {
         let mut est_pkt_size: usize = 4 + self.token.len() + 1 + 1 + self.payload.len();
 
         for option in &self.options {
-            est_pkt_size += 2 + option.value.len();
+            est_pkt_size += 2 + option.value_len();
 
-            if option.number.as_u16() >= 65000 {
+            if option.number() >= 65000 {
                 return Err(Error::MessageFormat);
             }
         }
@@ -385,34 +559,11 @@ impl Message {
             pkt.push(*byte)
         }
 
-        let last_option_number = 0;
+        let mut last_option_number = 0;
 
         for option in &self.options {
-            let start_index = pkt.len();
-            pkt.push(0);
-
-            if option.number.as_u16() < last_option_number {
-                panic!("bad order");
-            }
-
-            let delta = option.number.as_u16() - last_option_number;
-            let base_delta = match delta {
-                0...12 => delta,
-                13...268 => {pkt.push((delta-13) as u8); 13},
-                269...64999 => {pkt.push(((delta-269) >> 8) as u8); pkt.push((delta-269) as u8); 14},
-                _ => unreachable!(),
-            } as u8;
-            let length = option.value.len();
-            let base_length = match length {
-                0...12 => delta,
-                13...268 => {pkt.push((length-13) as u8); 13},
-                269...64999 => {pkt.push(((length-269) >> 8) as u8); pkt.push((length-269) as u8); 14},
-                _ => panic!("option too big"),
-            } as u8;
-
-            pkt[start_index] = base_delta << 4 | base_length;
-
-            pkt.append(&mut option.value.clone());
+            pkt.append(&mut option.build_header(&mut last_option_number));
+            pkt.append(&mut option.value_as_bytes());
         }
 
         if self.payload.len() > 0 {
@@ -423,7 +574,6 @@ impl Message {
         Ok(pkt)
     }
 }
-
 
 
 #[test]
@@ -514,21 +664,40 @@ fn test_msg_parse_get_con_with_opts() {
     assert!(msg.mid == 0x0037);
     assert!(msg.token == []);
     assert!(msg.options == [
-        option::Option{
-            number: option::Number::UriPath,
-            value: vec![0x31,0x61]
-        },
-        option::Option{
-            number: option::Number::UriPath,
-            value: vec![0x74,0x65,0x6d,0x70]
-        },
-        option::Option{
-            number: option::Number::UriQuery,
-            value: vec![0x61,0x33,0x32,0x63,0x38,0x35,0x62,0x61,0x39,0x64,
-                        0x64,0x61,0x34,0x35,0x38,0x32,0x33,0x62,0x65,0x34,
-                        0x31,0x36,0x32,0x34,0x36,0x63,0x66,0x38,0x62,0x34,
-                        0x33,0x33,0x62,0x61,0x61,0x30,0x36,0x38,0x64,0x37]
-        },
+        option::Option::UriPath("1a".to_string()),
+        option::Option::UriPath("temp".to_string()),
+        option::Option::UriQuery("a32c85ba9dda45823be416246cf8b433baa068d7".to_string())
     ]);
     assert!(msg.payload == [0x39, 0x39]);
+}
+
+#[test]
+fn test_msg_encode_get_con_with_opts() {
+    let ref_bin = [0x40,0x02,0x00,0x37,0xb2,0x31,0x61,0x04,0x74,0x65,
+                   0x6d,0x70,0x4d,0x1b,0x61,0x33,0x32,0x63,0x38,0x35,
+                   0x62,0x61,0x39,0x64,0x64,0x61,0x34,0x35,0x38,0x32,
+                   0x33,0x62,0x65,0x34,0x31,0x36,0x32,0x34,0x36,0x63,
+                   0x66,0x38,0x62,0x34,0x33,0x33,0x62,0x61,0x61,0x30,
+                   0x36,0x38,0x64,0x37,0xFF,0x39,0x39];
+    let msg = Message{
+        version: 1,
+        mtype: Mtype::Confirmable,
+        code: Code::Post,
+        mid: 0x0037,
+        token: vec![],
+        options: vec![
+            option::Option::UriPath("1a".to_string()),
+            option::Option::UriPath("temp".to_string()),
+            option::Option::UriQuery("a32c85ba9dda45823be416246cf8b433baa068d7".to_string())
+        ],
+        payload: vec![0x39, 0x39]
+    };
+
+    let test_bin = msg.to_bytes().unwrap();
+
+    assert!(test_bin.len() == ref_bin.len());
+
+    for i in 0..ref_bin.len() {
+        assert_eq!(test_bin[i], ref_bin[i]);
+    }
 }
