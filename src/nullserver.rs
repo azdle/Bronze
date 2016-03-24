01724eaaedc8d2b63b3024bafc4f3f6fb4e56827
Patrick Barrett <patrick@mkii.org>
2016-03-23 19:52:23 -0500

    reply only when message should be replied to

--- src/nullserver.rs
+++ src/nullserver.rs
@@ -9,16 +9,24 @@ impl RequestHandler for NullServer {
     fn handle_request(&self, _addr: &SocketAddr, request: &Message) -> Option<Vec<u8>> {
         println!("{:?}", request);
         // todo: would it be better to use an ack w/ error code?
-        let reply = Message{
-            version: 1,
-            mtype: Mtype::Reset,
-            code: Code::Empty,
-            mid: request.mid,
-            token: request.token.clone(),
-            options: vec![],
-            payload: vec![]
-        };
+        match request.mtype {
+            Mtype::Confirmable | Mtype::NonConfirmable => {
+                let reply = Message{
+                    version: 1,
+                    mtype: Mtype::Reset,
+                    code: Code::Empty,
+                    mid: request.mid,
+                    token: request.token.clone(),
+                    options: vec![],
+                    payload: vec![]
+                };
 
-        Some(reply.to_bytes().unwrap())
+                Some(reply.to_bytes().unwrap())
+            },
+            _ => {
+                println!("Not replying to message of type: {:?}", request.mtype);
+                None
+            }
+        }
     }
 }
